7cb27fbb39592ead41757e607cc10db905ff17fa
dev05 <dev05@dev.co-mit.com>
2025-07-23 15:55:10 +0700

    connect db mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    // Load biến môi trường từ .env
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    // Kết nối DB bằng TypeORM + ConfigService
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: +(configService.get<number>('DB_PORT') ?? 3306),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true, // Dùng khi dev
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
