9eefc7f1b14f725cd6172cf2e8168a06990c08cf
dev05 <dev05@dev.co-mit.com>
2025-07-25 15:41:35 +0700

    crawl data in otruyen

--- src/crawler/crawler.service.ts
+++ src/crawler/crawler.service.ts
@@ -4,10 +4,39 @@ import axios from 'axios';
 @Injectable()
 export class CrawlerService {
   async crawlAllStory(): Promise<void> {
-    console.log('>>> Bắt đầu crawlAllStory()');
-    const responseListStory = await axios.get(
-      'https://otruyenapi.com/v1/api/home',
-    );
-    console.log('crawl list story');
+    for (let page = 1; page <= 10; page++) {
+      const response = await axios.get(
+        `https://otruyenapi.com/v1/api/danh-sach/truyen-moi?page=${page}`,
+      );
+
+      const items = response.data.data.items;
+
+      for (const story of items) {
+        const slug = story.slug;
+        await this.crawlStoryDetail(slug);
+      }
+    }
+  }
+
+  async crawlStoryDetail(slug: string): Promise<void> {
+    try {
+      const response = await axios.get(
+        `https://otruyenapi.com/v1/api/truyen-tranh/${slug}`,
+      );
+
+      const storyDetail = response.data?.data.item;
+      console.log({
+        slug: storyDetail.slug,
+        name: storyDetail.name,
+        author: storyDetail.author,
+        categories: storyDetail.category?.map((c) => c.name),
+        status: storyDetail.status,
+        thumb_url: storyDetail.thumb_url,
+      });
+
+      // TODO: Lưu vào DB nếu cần
+    } catch (error) {
+      console.error(`❌ Lỗi khi crawl chi tiết truyện "${slug}":`, error.message);
+    }
   }
 }
